b44f129e5b1d3b7eb9897a1119dfbda1d426c6dc
Tommac <mcinteete@outlook.com>
2026-05-25 14:15:45 -0400

    Fix duplicate BrowserWindow width and height properties

--- apps/electron-backend/src/app/app.ts
+++ apps/electron-backend/src/app/app.ts
@@ -90,12 +90,13 @@ export default class App {
         const defaultHeight = Math.min(720, workAreaSize.height || 720);
 
         const savedWindowBounds = store.get(WINDOW_BOUNDS);
+        const { width, height, ...restSavedWindowBounds } = savedWindowBounds ?? {};
 
         // Create the browser window.
         App.mainWindow = new BrowserWindow({
             title: 'iptvmate',
             show: false,
-            ...(savedWindowBounds ?? {}),
+            ...restSavedWindowBounds,
             webPreferences: {
                 contextIsolation: true,
                 backgroundThrottling: false,
@@ -103,8 +104,8 @@ export default class App {
             },
             minHeight: 600,
             minWidth: 900,
-            width: savedWindowBounds?.width ?? defaultWidth,
-            height: savedWindowBounds?.height ?? defaultHeight,
+            width: width ?? defaultWidth,
+            height: height ?? defaultHeight,
             ...(process.platform === 'darwin'
                 ? {
                       titleBarStyle: 'hidden',
